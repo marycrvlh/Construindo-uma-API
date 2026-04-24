a7143785cf9175941a0256ab40b59a8b73a0bff9
Mariane Carvalho Dearo <marianecarvalhodearo@gmail.com>
2026-04-24 10:16:18 -0300

    --

--- api/medias.js
+++ api/medias.js
@@ -18,12 +18,20 @@ const medias = [
         id:4,
         type: "serie",
         title: "Game of Thrones"
-    }
+    },
 ]
 
 module.exports = (req, res) => {
     if (req.method === "GET"){
         return res.status(200).json({medias});
-
+    }else if (req.method === "POST"){
+        const newMedia = req.body;
+        return res.status(201).json({
+            message: "Media Created",
+            media: newMedia,
+            totalMedias: [...medias, newMedia].length,
+            allMedias:[...medias, newMedia]
+        }
+        );
     }
 }
